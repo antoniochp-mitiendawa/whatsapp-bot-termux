b4b61f06da24158a345b33af78360f60d8b5d90a
antoniochp-mitiendawa <antonio.chp@gmail.com>
2026-03-06 13:50:55 -0600

    Delete whatsapp-bot/update-baileys.sh

--- whatsapp-bot/update-baileys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "$(date): Iniciando actualización programada de Baileys..." >> /storage/emulated/0/WhatsAppBot/logs/updates.log
-pkill -f "node bot.js"
-sleep 3
-cd /data/data/com.termux/files/home/whatsapp-bot-termux/whatsapp-bot
-npm update @whiskeysockets/baileys
-cd /data/data/com.termux/files/home/whatsapp-bot-termux/whatsapp-bot
-nohup node bot.js > /dev/null 2>&1 &
-echo "$(date): Bot reiniciado" >> /storage/emulated/0/WhatsAppBot/logs/updates.log
