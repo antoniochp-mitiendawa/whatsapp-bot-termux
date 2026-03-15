804b4ab7e1da6a440ca02d71af6dd1eb9606994a
antoniochp-mitiendawa <antonio.chp@gmail.com>
2026-03-15 17:26:01 -0600

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "===================================="
-echo "🚀 INSTALADOR WHATSAPP BOT v47.0"
+echo "🚀 INSTALADOR WHATSAPP BOT v46.1"
 echo "===================================="
 echo ""
 
